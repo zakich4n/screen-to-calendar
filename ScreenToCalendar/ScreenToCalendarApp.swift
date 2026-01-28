6283284482c48db409b7fd29561e025648adf665
Zak Chan <zakigouizi@gmail.com>
2026-01-28 20:57:27 +0100

    Initial Commit

---
+++ ScreenToCalendar/ScreenToCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenToCalendarApp.swift
+//  ScreenToCalendar
+//
+//  Created by Zaki Gouizi on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScreenToCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
